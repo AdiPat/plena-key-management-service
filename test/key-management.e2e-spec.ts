feb693d59edac1cf9da530473673c7276c1cb2be
Aditya Patange <contact.adityapatange@gmail.com>
2024-06-14 20:36:24 +0530

    tests/e2e: add register user test

---
+++ test/key-management.e2e-spec.ts
@@ -0,0 +1,27 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { generateJWT } from './test-utils';
+
+describe('KeyManagementController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/keys/user/register (POST): registers user', () => {
+    const token = generateJWT();
+
+    return request(app.getHttpServer())
+      .post('/keys/user/register')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(HttpStatus.CREATED);
+  });
+});
