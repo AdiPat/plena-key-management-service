c489471b99e34e8702c9f923225d48deaad3737c
Aditya Patange <contact.adityapatange@gmail.com>
2024-06-14 20:45:18 +0530

    commons: extractClaims reads secret from env

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken';
 import { JwtClaims } from 'src/models';
 
-function extractClaims(token: string, secret: string): JwtClaims {
+function extractClaims(token: string): JwtClaims {
   try {
-    const decoded = jwt.verify(token, secret);
+    const decoded = jwt.verify(token, process.env.JWT_SIGNING_KEY);
     return decoded as JwtClaims;
   } catch (err) {
     console.error('Failed to verify token', err);
