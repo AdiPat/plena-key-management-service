8af45e043bc02dd1effbeec550a6ee0d526d1b12
Aditya Patange <contact.adityapatange@gmail.com>
2024-06-14 20:43:45 +0530

    scripts: move generateJWT to index

--- scripts/gen-token.ts
+++ scripts/gen-token.ts
@@ -1,27 +1,10 @@
-import jwt from 'jsonwebtoken';
-import { v4 as uuid } from 'uuid';
 import dotenv from 'dotenv';
+import { generateJWT } from '.';
 
 dotenv.config();
-
-function generateJWT(): string {
-  const payload = {
-    data: {
-      userId: uuid(),
-    },
-  };
-
-  const secret = process.env.JWT_SIGNING_KEY;
-
-  const options = {
-    expiresIn: '1h',
-  };
-
-  const token = jwt.sign(payload, secret, options);
-
-  return token;
-}
 
 const token = generateJWT();
 
 console.log('Generated JWT: ', token);
+
+export { generateJWT };
