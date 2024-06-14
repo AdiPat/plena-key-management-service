dc983747d24f0a45c64cf3ca84d5cc02fa8c0cd8
Aditya Patange <contact.adityapatange@gmail.com>
2024-06-14 19:21:08 +0530

    prisma: update schema and migrations

---
+++ prisma/migrations/20240614134831_dev/migration.sql
@@ -0,0 +1,13 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME
+);
+INSERT INTO "new_User" ("createdAt", "id", "updatedAt") SELECT "createdAt", "id", "updatedAt" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
