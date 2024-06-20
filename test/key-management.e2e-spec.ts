d108dd1b55fe6afd960d1ea49bce7aebc30d6f4a
Aditya Patange <contact.adityapatange@gmail.com>
2024-06-20 09:15:34 +0530

    tests/e2e: add keys api tests

--- test/key-management.e2e-spec.ts
+++ test/key-management.e2e-spec.ts
@@ -4,7 +4,7 @@ import request from 'supertest';
 import { AppModule } from '../src/core/app.module';
 import { generateJWT } from './test-utils';
 
-describe('KeyManagementController (e2e)', () => {
+describe('KeyManagement API (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -29,5 +29,173 @@ describe('KeyManagementController (e2e)', () => {
     return request(app.getHttpServer())
       .post('/keys/user/register')
       .expect(HttpStatus.UNAUTHORIZED);
+  });
+
+  it('/keys/ (POST): generates API key successfully', async () => {
+    const token = generateJWT();
+
+    // create user first
+    await request(app.getHttpServer())
+      .post('/keys/user/register')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(HttpStatus.CREATED);
+
+    return request(app.getHttpServer())
+      .post('/keys/')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(HttpStatus.CREATED)
+      .expect((res) => {
+        if (!(res.body.accessKey && res.body.rateLimits)) {
+          throw new Error('missing accessKey or rateLimits');
+        }
+      });
+  });
+
+  it('/keys/all (GET): lists all created keys', async () => {
+    const token = generateJWT();
+
+    await request(app.getHttpServer())
+      .post('/keys/user/register')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(HttpStatus.CREATED);
+
+    await request(app.getHttpServer())
+      .post('/keys/')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(HttpStatus.CREATED)
+      .expect((res) => {
+        if (!(res.body.accessKey && res.body.rateLimits)) {
+          throw new Error('missing accessKey or rateLimits');
+        }
+      });
+
+    const response = await request(app.getHttpServer())
+      .post('/keys/')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(HttpStatus.CREATED)
+      .expect((res) => {
+        if (!(res.body.accessKey && res.body.rateLimits)) {
+          throw new Error('missing accessKey or rateLimits');
+        }
+      });
+
+    const accessKey = response.body.accessKey;
+
+    return request(app.getHttpServer())
+      .get('/keys/all')
+      .set('x-api-key', `${accessKey.key}`)
+      .expect(HttpStatus.OK)
+      .expect((res) => {
+        if (res.body.length != 2) {
+          throw new Error('failed to list all access keys');
+        }
+      });
+  });
+
+  it('/keys/:keyId/rate-limit (POST): updates rate limits', async () => {
+    const token = generateJWT();
+
+    await request(app.getHttpServer())
+      .post('/keys/user/register')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(HttpStatus.CREATED);
+
+    const response = await request(app.getHttpServer())
+      .post('/keys/')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(HttpStatus.CREATED)
+      .expect((res) => {
+        if (!(res.body.accessKey && res.body.rateLimits)) {
+          throw new Error('missing accessKey or rateLimits');
+        }
+      });
+
+    const accessKey = response.body.accessKey;
+
+    return request(app.getHttpServer())
+      .post(`/keys/${accessKey.id}/rate-limit`)
+      .set('x-api-key', `${accessKey.key}`)
+      .send({ limit: 100 })
+      .expect(HttpStatus.OK);
+  });
+
+  it('/keys/:keyId/rate-limit (POST): returns bad request if keyId or limit is missing', async () => {
+    const token = generateJWT();
+
+    await request(app.getHttpServer())
+      .post('/keys/user/register')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(HttpStatus.CREATED);
+
+    const response = await request(app.getHttpServer())
+      .post('/keys/')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(HttpStatus.CREATED)
+      .expect((res) => {
+        if (!(res.body.accessKey && res.body.rateLimits)) {
+          throw new Error('missing accessKey or rateLimits');
+        }
+      });
+
+    const accessKey = response.body.accessKey;
+
+    return request(app.getHttpServer())
+      .post(`/keys/${accessKey.id}/rate-limit`)
+      .set('x-api-key', `${accessKey.key}`)
+      .send({ limit: 0 })
+      .expect(HttpStatus.BAD_REQUEST);
+  });
+
+  it('/keys/:keyId/expiry (POST): updates expiry', async () => {
+    const token = generateJWT();
+
+    await request(app.getHttpServer())
+      .post('/keys/user/register')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(HttpStatus.CREATED);
+
+    const response = await request(app.getHttpServer())
+      .post('/keys/')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(HttpStatus.CREATED)
+      .expect((res) => {
+        if (!(res.body.accessKey && res.body.rateLimits)) {
+          throw new Error('missing accessKey or rateLimits');
+        }
+      });
+
+    const accessKey = response.body.accessKey;
+
+    return request(app.getHttpServer())
+      .post(`/keys/${accessKey.id}/expiry`)
+      .set('x-api-key', `${accessKey.key}`)
+      .send({ expiry: new Date() })
+      .expect(HttpStatus.OK);
+  });
+
+  it('/keys/:keyId/revoke (POST): revokes key', async () => {
+    const token = generateJWT();
+
+    await request(app.getHttpServer())
+      .post('/keys/user/register')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(HttpStatus.CREATED);
+
+    const response = await request(app.getHttpServer())
+      .post('/keys/')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(HttpStatus.CREATED)
+      .expect((res) => {
+        if (!(res.body.accessKey && res.body.rateLimits)) {
+          throw new Error('missing accessKey or rateLimits');
+        }
+      });
+
+    const accessKey = response.body.accessKey;
+
+    return request(app.getHttpServer())
+      .post(`/keys/${accessKey.id}/revoke`)
+      .set('x-api-key', `${accessKey.key}`)
+      .expect(HttpStatus.OK);
   });
 });
