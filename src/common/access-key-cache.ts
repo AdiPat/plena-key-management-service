aa864bc70217cfe578a48d2b8d3c8f1fc8e283db
Aditya Patange <contact.adityapatange@gmail.com>
2024-06-20 08:05:43 +0530

    access-key-cache: add getAllAccessKeys() and update key space string

--- src/common/access-key-cache.ts
+++ src/common/access-key-cache.ts
@@ -1,4 +1,4 @@
-import { AccessKeyDetails } from 'src/models';
+import { AccessKeyDetails } from '../models';
 import { redis } from '.';
 
 class AccessKeyCache_ {
@@ -7,7 +7,7 @@ class AccessKeyCache_ {
   ): Promise<boolean> {
     try {
       const json = JSON.stringify(accessKeyDetails);
-      await redis.set(accessKeyDetails.accessKey, json);
+      await redis.set(`accessKeyDetails:${accessKeyDetails.accessKey}`, json);
       return true;
     } catch (err) {
       console.error('Failed to set access key details', err);
@@ -15,13 +15,24 @@ class AccessKeyCache_ {
     }
   }
 
-  async getAccessKeyDetails(accessKey: string): Promise<AccessKeyDetails> {
+  async getAccessKeyDetails(accessKeyValue: string): Promise<AccessKeyDetails> {
     try {
-      const json = await redis.get(accessKey);
+      const json = await redis.get(`accessKeyDetails:${accessKeyValue}`);
       return JSON.parse(json);
     } catch (err) {
       console.error('Failed to get access key details', err);
       return null;
+    }
+  }
+
+  async getAllAccessKeys(): Promise<AccessKeyDetails[]> {
+    try {
+      const keys = await redis.keys('accessKeyDetails:*');
+      const accessKeys = await redis.mget(keys);
+      return accessKeys.map((accessKey) => JSON.parse(accessKey));
+    } catch (err) {
+      console.error('Failed to get all access keys', err);
+      return [];
     }
   }
 }
